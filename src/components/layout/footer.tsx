3307f81ade1352030b023fd1e1a6b5fb256e9625
CHAUDHARY MOHAMMED HAMID <Hamid.Chaudhary25@gmail.com>
2025-09-07 07:54:20 +0000

    in footer add one more no 7021181068

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -52,6 +52,10 @@ const Footer = () => {
                 <span className="text-white">+91 9820409988</span>
             </li>
             <li className="flex items-center">
+                <Phone className="mr-3 h-5 w-5 text-accent" />
+                <span className="text-white">+91 7021181068</span>
+            </li>
+            <li className="flex items-center">
                 <Mail className="mr-3 h-5 w-5 text-accent" />
                 <span className="text-white">info@inshaindustries.com</span>
             </li>
