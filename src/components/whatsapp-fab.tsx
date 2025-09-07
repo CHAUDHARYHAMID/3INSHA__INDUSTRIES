c20c3e0e254abb859382c063c7998f1c6309600c
CHAUDHARY MOHAMMED HAMID <Hamid.Chaudhary25@gmail.com>
2025-09-07 07:21:08 +0000

    now just add background to the whasapp iamge for more clearity

--- src/components/whatsapp-fab.tsx
+++ src/components/whatsapp-fab.tsx
@@ -10,8 +10,8 @@ const WhatsAppFAB = () => {
       className="fixed bottom-6 right-6 z-50"
       suppressHydrationWarning
     >
-      <div className="relative h-16 w-16 flex items-center justify-center">
-        <FaWhatsapp className="h-16 w-16 text-green-500" />
+      <div className="relative h-16 w-16 flex items-center justify-center rounded-full bg-white shadow-lg">
+        <FaWhatsapp className="h-12 w-12 text-green-500" />
         <span className="sr-only">Contact us on WhatsApp</span>
       </div>
     </Link>
