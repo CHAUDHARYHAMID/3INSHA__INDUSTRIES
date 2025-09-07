112b49235b3924c92c1636b8f9194fecec066486
CHAUDHARY MOHAMMED HAMID <Hamid.Chaudhary25@gmail.com>
2025-09-07 07:55:29 +0000

    just use | and add after that dont use next line

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -49,11 +49,7 @@ const Footer = () => {
             </li>
             <li className="flex items-center">
                 <Phone className="mr-3 h-5 w-5 text-accent" />
-                <span className="text-white">+91 9820409988</span>
-            </li>
-            <li className="flex items-center">
-                <Phone className="mr-3 h-5 w-5 text-accent" />
-                <span className="text-white">+91 7021181068</span>
+                <span className="text-white">+91 9820409988 | +91 7021181068</span>
             </li>
             <li className="flex items-center">
                 <Mail className="mr-3 h-5 w-5 text-accent" />
