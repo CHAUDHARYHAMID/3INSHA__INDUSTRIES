7269499be1011f9bdda5f8f8ebf0c050a7d0a70e
CHAUDHARY MOHAMMED HAMID <Hamid.Chaudhary25@gmail.com>
2025-09-08 06:41:39 +0000

    move the logo of insha industries right side 1 cm

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -55,7 +55,7 @@ const Header = () => {
       {/* Main Header */}
       <div className="bg-background">
         <div className="container mx-auto flex h-20 items-center justify-between px-4 sm:px-6 lg:px-8">
-          <Link href="#home" className="flex-shrink-0">
+          <Link href="#home" className="flex-shrink-0 lg:ml-4">
              <Image src="https://i.postimg.cc/sgXrrChv/Whats-App-Image-2025-09-05-at-18-39-09-fd785157.jpg" alt="Insha Industries Logo" width={200} height={56} className="h-16 w-auto" />
           </Link>
 
