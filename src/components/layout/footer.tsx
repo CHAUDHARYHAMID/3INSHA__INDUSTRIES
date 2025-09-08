01670179425354eb9c938c7208b8e51032e6eed1
CHAUDHARY MOHAMMED HAMID <Hamid.Chaudhary25@gmail.com>
2025-09-08 13:26:02 +0000

    Link: https://postimg.cc/zVgCfQKH Direct link: https://i.postimg.cc/DyCB

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -83,6 +83,15 @@ const Footer = () => {
                         className='object-contain h-full w-auto'
                         data-ai-hint="certification logos" />
                 </div>
+                <div className="relative h-16 w-auto overflow-hidden rounded-lg">
+                    <Image 
+                        src="https://i.postimg.cc/DyCBxkz6/4-footer-image.png" 
+                        alt="Certifications" 
+                        width={63}
+                        height={63}
+                        className='object-contain h-full w-auto'
+                        data-ai-hint="certification logos" />
+                </div>
              </div>
           </div>
 
