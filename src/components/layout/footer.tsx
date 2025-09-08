2c18fe4f0256d3c9147b15676199ec335fca7c12
CHAUDHARY MOHAMMED HAMID <Hamid.Chaudhary25@gmail.com>
2025-09-08 13:30:37 +0000

    now its too short adjust it according all three

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -83,7 +83,7 @@ const Footer = () => {
                         className='object-contain h-full w-auto'
                         data-ai-hint="certification logos" />
                 </div>
-                <div className="relative rounded-lg bg-white p-1">
+                <div className="relative h-16 w-auto flex items-center justify-center rounded-lg bg-white p-1">
                     <Image 
                         src="https://i.postimg.cc/DyCBxkz6/4-footer-image.png" 
                         alt="Certifications" 
