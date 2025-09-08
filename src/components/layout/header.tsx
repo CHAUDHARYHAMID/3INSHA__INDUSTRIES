5e29f99e87b867c205d571254b9abc57eefbc783
CHAUDHARY MOHAMMED HAMID <Hamid.Chaudhary25@gmail.com>
2025-09-08 10:37:11 +0000

    now move all the elemnts and the logo in the right side by 1cm

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -43,7 +43,7 @@ const Header = () => {
 
       {/* Main Header */}
       <div className="bg-background">
-        <div className="mx-auto flex h-24 items-center justify-between px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto flex h-24 items-center justify-between px-4 sm:px-6 lg:px-8 ml-10">
           <div className="flex items-center gap-x-12">
             <Link href="#home" className="flex-shrink-0">
                <Image src="https://i.postimg.cc/WzHVzDwK/Whats-App-Image-2025-09-05-at-18-39-09-fd785157-Photoroom.png" alt="Insha Industries Logo" width={420} height={120} />
