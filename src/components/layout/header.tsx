4b512cc0e578c591a64bc16da49a3007f5bf623c
CHAUDHARY MOHAMMED HAMID <Hamid.Chaudhary25@gmail.com>
2025-09-08 07:51:49 +0000

    move the logo by 1cm on the right side in the header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -44,8 +44,8 @@ const Header = () => {
       {/* Main Header */}
       <div className="bg-background">
         <div className="mx-auto flex h-24 items-center justify-between px-4 sm:px-6 lg:px-8">
-          <Link href="#home" className="flex-shrink-0 lg:ml-14">
-             <Image src="https://i.postimg.cc/WzHVzDwK/Whats-App-Image-2025-09-05-at-18-39-09-fd785157-Photoroom.png" alt="Insha Industries Logo" width={280} height={68} />
+          <Link href="#home" className="flex-shrink-0 lg:ml-20">
+             <Image src="https://i.postimg.cc/WzHVzDwK/Whats-App-Image-2025-09-05-at-18-39-09-fd785157-Photoroom.png" alt="Insha Industries Logo" width={300} height={80} />
           </Link>
 
           {/* Desktop Navigation */}
@@ -79,7 +79,7 @@ const Header = () => {
                 <div className="flex h-full flex-col">
                   <div className="flex items-center justify-between border-b p-4">
                      <Link href="#home" onClick={() => setMobileMenuOpen(false)}>
-                        <Image src="https://i.postimg.cc/WzHVzDwK/Whats-App-Image-2025-09-05-at-18-39-09-fd785157-Photoroom.png" alt="Insha Industries Logo" width={200} height={52} />
+                        <Image src="https://i.postimg.cc/WzHVzDwK/Whats-App-Image-2025-09-05-at-18-39-09-fd785157-Photoroom.png" alt="Insha Industries Logo" width={220} height={60} />
                     </Link>
                     <SheetTrigger asChild>
                       <Button variant="ghost" size="icon">
